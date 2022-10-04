8d16babea2b7cb68d5145495bfb905502bfdb379
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-10-03 21:16:22 -0500

    updated footer

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return render_template('home.html')
+
+@app.route("/data_entry")
+def data_entry():
+    return render_template('data_entry.html')
