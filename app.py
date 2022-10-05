fe5a97aba71dea064c212eb5029d2d0bb0af8862
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-10-05 13:52:38 -0500

    started enter new data page

--- app.py
+++ app.py
@@ -8,4 +8,31 @@ def home():
 
 @app.route("/data_entry")
 def data_entry():
-    return render_template('data_entry.html')
+    stakeholders = ['Jason Hegelmeyer',
+                    'Donovan Cusick',
+                    'Steph Shields',
+                    'Valeria Rodriguez',
+                    'Jo Scaletty',
+                    'Molly Whalen',
+                    'Armaan Ajani',
+                    'Sadie Bernstein',
+                    'Alexis Schwartz',
+                    'Joshua Gregory',
+                    'Marcos Rios',
+                    'Felix Beilin',
+                    'Brian Whetsell',
+                    'Zai Dawodu',
+                    'Julia Karten',
+                    'Sam Douki',
+                    'Sohae Yang',
+                    'Sara Azimipour',
+                    'Dylan Jost',
+                    'Leah Ryzenman',
+                    'Dalia Segal-Miller']
+    admin =  ['Fill me',
+               'Ask Molly']
+    return render_template('data_entry.html',stakeholders=stakeholders,admin=admin)
+
+@app.route("/data_submitted")
+def data_submitted():
+    return render_template('home.html')
