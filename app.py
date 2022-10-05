c42374bde35c8ff2d4634388197271f6914a30f3
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-10-05 17:34:37 -0500

    first draft of form done, still need to implement some JS

--- app.py
+++ app.py
@@ -2,35 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+stakeholders = ['Jason Hegelmeyer',
+                'Donovan Cusick',
+                'Steph Shields',
+                'Valeria Rodriguez',
+                'Jo Scaletty',
+                'Molly Whalen',
+                'Armaan Ajani',
+                'Sadie Bernstein',
+                'Alexis Schwartz',
+                'Joshua Gregory',
+                'Marcos Rios',
+                'Felix Beilin',
+                'Brian Whetsell',
+                'Zai Dawodu',
+                'Julia Karten',
+                'Sam Douki',
+                'Sohae Yang',
+                'Sara Azimipour',
+                'Dylan Jost',
+                'Leah Ryzenman',
+                'Dalia Segal-Miller']
+admin =  ['Fill me',
+          'Ask Molly']
+
 @app.route("/")
 def home():
     return render_template('home.html')
 
 @app.route("/data_entry")
 def data_entry():
-    stakeholders = ['Jason Hegelmeyer',
-                    'Donovan Cusick',
-                    'Steph Shields',
-                    'Valeria Rodriguez',
-                    'Jo Scaletty',
-                    'Molly Whalen',
-                    'Armaan Ajani',
-                    'Sadie Bernstein',
-                    'Alexis Schwartz',
-                    'Joshua Gregory',
-                    'Marcos Rios',
-                    'Felix Beilin',
-                    'Brian Whetsell',
-                    'Zai Dawodu',
-                    'Julia Karten',
-                    'Sam Douki',
-                    'Sohae Yang',
-                    'Sara Azimipour',
-                    'Dylan Jost',
-                    'Leah Ryzenman',
-                    'Dalia Segal-Miller']
-    admin =  ['Fill me',
-               'Ask Molly']
     return render_template('data_entry.html',stakeholders=stakeholders,admin=admin)
 
 @app.route("/data_submitted")
