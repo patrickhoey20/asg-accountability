dc2d989399bee0119cdd3b5cc0c183b7f32040be
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-10-06 18:07:34 -0500

    heroku db up and running

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zzgbanrolxeoql:8d9526e98359fc9b676443479d91ef329343b06010795271cedaa32be19a5693@ec2-3-214-2-141.compute-1.amazonaws.com:5432/dlglent4m7pik'
 db = SQLAlchemy(app)
 
-class Accountability(db.Model):
-    __tablename__ = "accoutability"
-    id = db.Column(db.Integer, primary_key=True)
-    stakeholder = db.Column(db.String)
-    
-    def __init__(self, stakeholder):
-        self.stakeholder = stakeholder
-
 with app.app_context():
     db.create_all()
 
@@ -53,7 +45,7 @@ def data_entry():
 def data_submitted():
     if request.method == 'POST':
         stakeholder = request.form['stakeholder']
-        reg = Accountability(stakeholder)
-        db.session.add(reg)
-        db.session.commit()
+        db.execute("INSERT INTO accountability (stakeholder) VALUES (:stakeholder)",
+              {"stakeholder":stakeholder})
+        db.commit()
     return render_template('home.html')
