23fc9e47657aa072596376d8c3e7c45bc11025f1
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-10-06 18:40:23 -0500

    db working now, need to add all cols

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zzgbanrolxeoql:8d9526e98359fc9b676443479d91ef329343b06010795271cedaa32be19a5693@ec2-3-214-2-141.compute-1.amazonaws.com:5432/dlglent4m7pik'
 db = SQLAlchemy(app)
 
+class Accountability(db.Model):
+    __tablename__ = "accoutability"
+    id = db.Column(db.Integer, primary_key=True)
+    stakeholder = db.Column(db.String)
+    date = db.Column(db.Date)
+    num_meetings_admin_since_last = db.Column(db.Integer)
+
+    def __init__(self, stakeholder, date, num_meetings_admin_since_last):
+        self.stakeholder = stakeholder
+        self.date = date
+        self.num_meetings_admin_since_last = num_meetings_admin_since_last
+
 with app.app_context():
     db.create_all()
 
@@ -45,7 +57,9 @@ def data_entry():
 def data_submitted():
     if request.method == 'POST':
         stakeholder = request.form['stakeholder']
-        db.execute("INSERT INTO accountability (stakeholder) VALUES (:stakeholder)",
-              {"stakeholder":stakeholder})
-        db.commit()
+        date = request.form['date']
+        num_meetings_admin_since_last = request.form['num_meetings_admin_since_last']
+        reg = Accountability(stakeholder,date,num_meetings_admin_since_last)
+        db.session.add(reg)
+        db.session.commit()
     return render_template('home.html')
