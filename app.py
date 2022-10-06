63f89dd0351f0abd48456a25069ce9e629231769
patrickhoey20 <patrickhoey2024@u.northwestern.edu>
2022-10-06 16:13:35 -0500

    trying to add database

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/asg-data'
+db = SQLAlchemy(app)
+
+class Accountability(db.Model):
+    __tablename__ = "accoutability"
+    id = db.Column(db.Integer, primary_key=True)
+    stakeholder = db.Column(db.String)
+    
+    def __init__(self, stakeholder):
+        self.stakeholder = stakeholder
+
+with app.app_context():
+    db.create_all()
 
 stakeholders = ['Jason Hegelmeyer',
                 'Donovan Cusick',
@@ -34,6 +48,11 @@ def home():
 def data_entry():
     return render_template('data_entry.html',stakeholders=stakeholders,admin=admin)
 
-@app.route("/data_submitted")
+@app.route("/data_submitted", methods=['POST'])
 def data_submitted():
+    if request.method == 'POST':
+        stakeholder = request.form['stakeholder']
+        reg = Accountability(stakeholder)
+        db.session.add(reg)
+        db.session.commit()
     return render_template('home.html')
